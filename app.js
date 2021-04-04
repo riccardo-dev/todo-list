f8fab86febea7c31016516bcbb877c40685dc393
Riccardo Andria <andria.riccardo@LIVE.IT>
2021-04-05 01:08:02 +0200

    adding some packages

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
 const app = express();
 const port = 8080;
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
     res.send('Hello World')
