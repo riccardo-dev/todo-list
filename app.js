5ae14051645df01c924f617e20645b4abdc14817
Riccardo Andria <andria.riccardo@LIVE.IT>
2021-04-05 01:34:31 +0200

    connect to  mongodb

--- app.js
+++ app.js
@@ -2,9 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-
 const app = express();
 const port = 8080;
+
+const MONGODB_URI =  "mongodb+srv://admin:iQgKf3gL4tT28Pwb@cluster0.owg0z.mongodb.net/todo?retryWrites=true&w=majority";
+
+
+//Connect Database
+mongoose.connect(
+    MONGODB_URI
+).then(result => {
+    console.log('CONNECTED');
+}
+
+)
 
 app.use(bodyParser.urlencoded({extended: false}));
 
