3c4733ca7ebb5291f4c4aec7b7a305b0251afd23
riccardo-dev <r.andriadev@gmail.com>
2021-05-09 00:32:01 +0200

    change the axios baseURL

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:8080'
+axios.defaults.baseURL = 'https://plan-day.herokuapp.com/'
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
 
 
