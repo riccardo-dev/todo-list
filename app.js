86df44e4dd855b6538c606cc66c852a7a6e49b43
riccardo-dev <r.andriadev@gmail.com>
2021-05-10 09:22:02 +0200

    building client side

--- app.js
+++ app.js
@@ -48,10 +48,10 @@ app.use('/users', userRoutes);
 app.use('/login', loginRoutes);
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('./client/build'));
+    app.use(express.static('client/build'));
     
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, './client', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     });
 } else {
     app.get('/', (req, res) => {
